3f2267e69236492e55ccece9cc7edae6fe05fc55
LimSeHyeon <dlatpgus1205@naver.com>
2024-04-30 14:23:04 +0900

    feat::closure 실습

--- closure.js
+++ closure.js
@@ -40,17 +40,22 @@ console.log(counter2());//3
 //시작 값과 단계 값을 받아서, 호출할 때마다 시작 값에서부터 단계 값만큼 증가하는 값을 반환하는 함수를 만드세요.
 function createIncremeter(start, step) {
     let count = start;
+    function inner2() {
+        console.log("hihihi");
+        return 1;
+    }
     function inner() {
         count += step;
         return count;
     }
-    return inner;
+    
+    return {inner, inner2};
 }
 
 const incByOne = createIncremeter(5,1);
-console.log(incByOne());//6
-console.log(incByOne());//7
+console.log(incByOne.inner());//6
+console.log(incByOne.inner());//7
 
 const incByTen = createIncremeter(10, 10);
-console.log(incByTen());//20
-console.log(incByTen());//30
+console.log(incByTen.inner2());//20
+console.log(incByTen.inner2());//30
