882f1dd6d6b440d1ae85d7b3bdeff3f0e32bc77f
LimSeHyeon <dlatpgus1205@naver.com>
2024-04-30 10:38:33 +0900

    feat::practice.js 연습용 파일 생성

---
+++ practice.js
@@ -0,0 +1,12 @@
+//사용자로부터 점수 3개 입력받아 모든 점수보다 65보다 클 경우 true, 아니면 false 출력하기
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// rl.on('line', function (line) {
+
+
+//     rl.close();
+// });
