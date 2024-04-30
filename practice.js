7ab499287eb9bf38b4765de383e2c797d257df7b
LimSeHyeon <dlatpgus1205@naver.com>
2024-04-30 11:04:19 +0900

    연습문제 풀어보기

--- practice.js
+++ practice.js
@@ -46,9 +46,28 @@ console.log(newStr);
 let arr1 = multi.split("H");
 console.log(arr1);
 */
-
+/*
 let str3 = "     Hello World        ";
 console.log(str3);
 console.log(str3.trim());
 
 console.log("*".repeat(10));
+*/
+
+/*
+연습문제
+a=" 90:30:80 "이고 :을 기준으로 각각 수학점수, 영어점수, 과학점수이다.
+문자열 함수를 이용해 공백을 제거하고,
+각 점수를 각각 math, english, science 변수에 저장한 후,
+average 란 변수에 평균값을 저장하고 출력하시오.
+이 때 출력의 형태는 “평균 점수는 ~~입니다.”로 하시오.
+*/
+let a = " 90:30:80 ";
+a = a.trim();//공백 제거
+let arr = a.split(":");
+let math, english, science;
+math = parseInt(arr[0]);
+english = parseInt(arr[1]);
+science = parseInt(arr[2]);
+let average = (math + english + science) / 3;
+console.log(`평균 점수는 ${average}입니다.`);
