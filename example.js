80f6f856dc88167d287a49e7bd86c84a1e4dce26
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-02 13:06:42 +0900

    for 반복문 연습

--- example.js
+++ example.js
@@ -1,3 +1,4 @@
+/*
 const score = [
     prompt("점수입력1"),
     prompt("점수입력2"),
@@ -11,3 +12,47 @@ if(browser === "Edge") {
 } else if(['Chrome', 'Firefox'].includes(browser)) {
     console.log('using another');
 }
+
+//안녕안녕
+let num = parseInt(prompt("정수를 입력해주세요 : "));
+for(let i = 0; i < num; i++) {
+    console.log("안녕");
+}
+
+//별찍기
+let num = parseInt(prompt("정수를 입력해주세요 : "));
+for(let i = 0; i <= num; i++) {
+    for(let j = 0; j < i; j++) {
+        console.log("*");
+    }
+    console.log("");
+}
+
+//별찍기(역순)
+let num = parseInt(prompt("정수를 입력해주세요 : "));
+for(let i = num; i > 0; i--) {
+    for(let j = 0; j < i; j++) {
+        console.log("*");
+    }
+    console.log("");
+}
+
+let x = [3,6,9,20,-7,5];
+for(let item in x) {
+    console.log(x[item]*10);
+    x[item] = x[item]*10;
+}*/
+
+/*
+let y = {'math': 70, 'science': 80, 'english': 20};
+for(let i in y) {
+    console.log(y[i]+10);
+    y[i] += 10;
+}
+console.log(y);
+
+
+//숫자 입력받고 구구단
+const input = parseInt(prompt("정수 입력 : "));
+for(let i = 1; i < 10; i++) console.log(`${input}*${i} = ${input*i}`);
+*/
--- for.js
+++ for.js
@@ -8,7 +8,10 @@ for(let i in obj1) {
 }
 console.log('-'.repeat(30));
 
-const arr1 = ['a', 'b', 'c'];
+const arr1 = ['a', 'b', 'c'];//in은 인덱스
 for(let i in arr1) {
     console.log(i);
+}
+for(let item of arr1) {//of는 값
+    console.log(item);
 }
