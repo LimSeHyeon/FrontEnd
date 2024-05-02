2bd3ea461a2be1df9d63445fe444e3d2a29fed28
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-02 10:13:09 +0900

    object 실습

---
+++ objects.js
@@ -0,0 +1,11 @@
+const KEY = 'SAMPLE';
+const johnProfile = {
+    name : "john",
+    sayHi: function(){
+        console.log(this.name,"친구야 반갑다.");
+    },
+    [KEY]: "sampleValue"
+}
+johnProfile.sayHi();
+console.log(johnProfile['sayHi']);
+console.log(johnProfile.SAMPLE);
