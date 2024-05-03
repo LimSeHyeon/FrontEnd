78a86d482f7d3b326eeb44ccbc52bc82d7d04017
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-03 13:23:52 +0900

    객체지향적 프로그래밍, 프로토타입

--- oop1.js
+++ oop1.js
@@ -35,3 +35,13 @@ const animal2 = new Animal('사자');
 console.log(animal2);
 console.log(animal2.constructor)
 animal2.run();
+
+
+//prototype : 생성자 함수로 만들어진 인스턴스가 가질 메소드를 정의
+Animal.prototype.eat = function() {
+    console.log(`${this.name}가 먹는다`);
+}
+
+animal2.eat();
+console.log(animal2);
+console.log(animal2.__proto__);//객체 본인의 생성자 함수 혹은 만들어진 생성자 함수의 프로토타입에 접근
