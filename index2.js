e9fe31d12f9a4db23b7f5f92821d42cfa6a64f24
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-07 08:20:26 +0900

    feat: 입력 2개 받아 합 출력하기

---
+++ index2.js
@@ -0,0 +1,26 @@
+/*
+function ask(agree, yes, no) {
+    agree() ? yes() : no();
+}
+
+ask(
+    () => confirm("동의하십니까?"),
+    function() { alert("동의하셨습니다.") },
+    function() { alert("취소 버튼을 누르셨습니다.") }
+);
+
+
+//아래 둘이 같음
+() => {
+    return confirm("동의?");
+}
+
+() => confirm("동의?");
+*/
+
+// const age = parseInt(prompt("나이를 입력하여 주세요."));
+// let message = (age<3) ? "무료입니다":
+//     (age<10) ? "청소년입니다." :
+//     (age<100) ? "감사합니다." :
+//     "나이가 아주 많으시군요"
+// alert(message);
