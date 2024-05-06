866bab51f11a395fba4ab0c8de866cd086fec5c9
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-07 08:43:58 +0900

    feat e.stopPropagation()

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ let outer = document.getElementById('outer');
 let inner = document.getElementById('inner');
 
 wrap.addEventListener('click', e=> {
+    e.stopPropagation();
     console.log("wrap Event");
 });
 
 outer.addEventListener('click', e=> {
+    e.stopPropagation();
     console.log("outer Event");
 });
 
 inner.addEventListener('click', e=> {
+    e.stopPropagation();
     console.log("inner Event");
 });
