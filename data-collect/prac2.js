f15004f3295a93c8fe8bd68ca5d07aafb6cf17ee
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-07 10:46:23 +0900

    feat: cheerio 설치

--- data-collect/prac2.js
+++ data-collect/prac2.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import * as cheerio from 'cheerio';
 
 axios.get('https://example.com').then(resp=>{
     return resp.data;
 }).then(data => {
     console.log(data);
+    const $ = cheerio.load(data);
 });
