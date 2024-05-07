d0aff3cda0eadf70c73856ff0c7da5997e4c3509
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-07 14:43:58 +0900

    feat: 10페이지 정보 수집

--- data-collect/example1.js
+++ data-collect/example1.js
@@ -2,32 +2,30 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 
 let quotes = [];
-axios.get('https://quotes.toscrape.com').then(resp => {
-    return resp.data;
-}).then(data => {
-    const $ = cheerio.load(data);
-    /*console.log(
-        // <cheerio객체>.find(<css selector>)
-        $(quoteTags[0]).find('.text').text()
-    );
-    */
+
+for( let idx = 1; idx <= 10; idx++ ) {
+    axios.get(`https://quotes.toscrape.com/page/${idx}`).then(resp => {
+        return resp.data;
+    }).then(data => {
+        const $ = cheerio.load(data);
+
+        const quoteTags = $('.container .quote');
+        const authorNameTags = $('.container .author');
+        const tagsTags = $('.container .tags');
     
-    const quoteTags = $('.container .quote');
-    const authorNameTags = $('.container .author');
-    const tagsTags = $('.container .tags');
-
-    for(let i = 0; i < tagsTags.length; i++) {
-        let tagList = [];
-        let tags = $(tagsTags[i]).find('.tag');
-        for(let j = 0; j < tags.length; j++) {
-            tagList.push($(tags[j]).text());
+        for(let i = 0; i < tagsTags.length; i++) {
+            let tagList = [];
+            let tags = $(tagsTags[i]).find('.tag');
+            for(let j = 0; j < tags.length; j++) {
+                tagList.push($(tags[j]).text());
+            }
+    
+            quotes.push({
+                "quote": $(quoteTags[i]).find('.text').text(),
+                "author": $(authorNameTags[i]).text(),
+                "tags": tagList
+            });
         }
-
-        quotes.push({
-            "quote": $(quoteTags[i]).find('.text').text(),
-            "author": $(authorNameTags[i]).text(),
-            "tags": tagList
-        });
-    }
-    console.log(quotes);
-});
+        if(idx===10) console.log(quotes);
+    });
+}
