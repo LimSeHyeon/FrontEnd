649a4b3947c347e7e537306c3ec4aadd9470c556
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-07 10:58:14 +0900

    feat: cheerio parsing

--- data-collect/prac2.js
+++ data-collect/prac2.js
@@ -4,6 +4,10 @@ import * as cheerio from 'cheerio';
 axios.get('https://example.com').then(resp=>{
     return resp.data;
 }).then(data => {
-    console.log(data);
+    // console.log(data);
     const $ = cheerio.load(data);
+    const header = $('h1');
+    console.log(header.text());
+    const pTags = $('div').children('p');
+    console.log(pTags.text());
 });
