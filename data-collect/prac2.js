4188322718370748565b1a6f0b661ef6edbc6665
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-07 11:17:32 +0900

    feat: 요소 배열 만들기 방식 2가지

--- data-collect/prac2.js
+++ data-collect/prac2.js
@@ -7,7 +7,26 @@ axios.get('https://example.com').then(resp=>{
     // console.log(data);
     const $ = cheerio.load(data);
     const header = $('h1');
-    console.log(header.text());
+    // console.log(header.text());
     const pTags = $('div').children('p');
-    console.log(pTags.text());
+    // console.log(pTags.text());
+
+    //get property
+    const url = $('a').prop('href');
+    console.log(url);
+
+    //map - for
+    const ptag = $('p');
+
+    const values = ptag.map((i, elem) => {
+        return $(elem).text();
+    });
+    // console.log(values);
+
+    const data2 = [];
+    for(let i = 0; i < values.length; i++) {
+        data2.push($(ptag[i]).text());
+    }
+    // console.log(data2);
+
 });
