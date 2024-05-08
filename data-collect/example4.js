8385c68bc2f2321eb128ddeecb8617c7585bbdce
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-08 13:41:49 +0900

    feat: 필요한 값 파싱까지

--- data-collect/example4.js
+++ data-collect/example4.js
@@ -13,11 +13,32 @@ async function scrapeFinance(code) {
 
     const data = await resp.data;
     const $ = cheerio.load(data);
-    const dayCost = $('.type2').text();
+    // const dayCost = $('.type2');
     // console.log(dayCost);
-    const result = JSON.stringify(dayCost);
+    // console.log(dayCost);
+    // return dayCost;
+
+    const trTags = $('tr[onmouseover]');
+    const result = trTags.map((i, elem) => {
+        const tdTags = $(elem).find('td>span');
+        return {
+            date: $(tdTags[0]).text(),
+            close: $(tdTags[1]).text(),
+            ratio: $(tdTags[2]).text().trim(),
+            open: $(tdTags[3]).text(),
+            high: $(tdTags[4]).text(),
+            low: $(tdTags[5]).text(),
+            volume: $(tdTags[6]).text()
+        }
+    }).get();
+    
     console.log(result);
-    return result;
+
+    // for(let i = 2; i < stockInfo.length-1; i++) {
+    //     if(i===7 || i===8 || i===9) continue;
+    //     const info = [];
+    //     let date = $(stockInfo[i]).find('.tah .p10 .gray03').text();
+    // }
 }
 
 scrapeFinance('005930');
