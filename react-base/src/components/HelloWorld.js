a1eb5f2da6698ce0b72b0c83fa56b2ab04721ff4
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-09 11:52:00 +0900

    feat: hello world 컴포넌트 제작

---
+++ react-base/src/components/HelloWorld.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HelloWorld() {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+            <p>This is My first React Application</p>
+        </div>
+    )
+}
+export default HelloWorld;
