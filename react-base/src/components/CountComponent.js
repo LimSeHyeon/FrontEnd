55d4fd83e0790c01fb86e938dea15e113cf73e8e
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-09 15:54:05 +0900

    feat: useEffect 탐구

--- react-base/src/components/CountComponent.js
+++ react-base/src/components/CountComponent.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState} from 'react';
 
 export default function CountComponent() {
     const [count, setCount] = useState(0);
@@ -12,6 +12,21 @@ export default function CountComponent() {
     // const addCount = () => {
     //     setCount(count+1);
     // }
+
+    //useEffect 인자 2개
+    // 첫번째 : 함수,
+    // 두번째 : dependency array : state 혹은 props => props가 변하면 함수 실행
+    // 리턴값 : 컴포넌트가 없어질 때, 혹은 새로 생길 때 실행하기 위한 함수
+    useEffect(()=> {
+        console.log("데이터 받아오기! (이 함수는 한 번만 실행됩니다.");
+        return () => {
+            console.log("메모리를 잡아먹으면 리소스 해제 하는 함수를 return 해 주어야 합니다.")
+        }
+    }, [])
+
+    useEffect(() => {
+        console.log(`카운트가 증가할 때마다 실행! \n -count: ${count}`);
+    }, [count])
     return (
         <div>
             <div>{count}</div>
