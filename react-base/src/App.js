6d9a090ce1d78ff39cf67419c196f86737d17eac
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-09 13:59:03 +0900

    feat: 이미지 캡션 컴포넌트 생성

--- react-base/src/App.js
+++ react-base/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import HelloWorld from './components/HelloWorld.js';
+import HelloWorld from './components/HelloWorld';
+import CaptionImage from './components/CaptionImage';
 
 function App() {
   return (
@@ -8,7 +9,11 @@ function App() {
     //   <HelloWorld style={{textAlign: "center"}}/>
     // </div>
     <div className="App">
-    <HelloWorld/>
+    {/* <HelloWorld/> */}
+    <CaptionImage imgUrl="https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/7KPi/image/UGSyDK8noVFVfhr0PUyridMp6T8.JPG"
+    caption="구름이미지"/>
+    <CaptionImage imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmsaOoe2KUxfphcXFyqy2q0LHCfMUIxbx8xA&s"
+    caption="이건 트럭입니다."/>
   </div>
   );
 }
