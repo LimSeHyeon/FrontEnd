6bde0c69a72bc9dbbff5bc1fb0db0378479c7ccc
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-10 09:24:50 +0900

    feat: useRef, useMemo 간단 사용

--- react-base/src/App.js
+++ react-base/src/App.js
@@ -6,6 +6,8 @@ import BlinkComponent from './components/BlinkComponent';
 import CBlinkComponent from './components/CBlinkComponent';
 import CountComponent from './components/CountComponent';
 import {useState} from 'react';
+import FocusInputButton from './components/RegisterInputButton';
+import PrimeCalculator from './components/CalculatePrimes';
 
 function App() {
   const [showCount, setShowCount] = useState(true);
@@ -15,12 +17,13 @@ function App() {
     //   <HelloWorld style={{textAlign: "center"}}/>
     // </div>
     <div className="App">
+      
     {/* <HelloWorld/> */}
     {/* <CaptionImage imgUrl="https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/7KPi/image/UGSyDK8noVFVfhr0PUyridMp6T8.JPG"
     caption="구름이미지"/>
     <CaptionImage imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmsaOoe2KUxfphcXFyqy2q0LHCfMUIxbx8xA&s"
     caption="이건 트럭입니다."/> */}
-    <BlinkComponent text="안녕하세요"/>
+    {/* <BlinkComponent text="안녕하세요"/> */}
     {/* <CBlinkComponent text="반갑습니다"/> */}
     {/* <CountComponent/> */}
 
@@ -28,6 +31,8 @@ function App() {
       setShowCount(!showCount);
     }}>버튼</button>
     {showCount? <CountComponent/> : null} */}
+    <FocusInputButton />
+    <PrimeCalculator/>
     
   </div>
   );
