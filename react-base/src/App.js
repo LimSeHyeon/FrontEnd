f412e133769cef6925c305d14cb9356479ac02d5
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-12 17:12:39 +0900

    feat: 입력하는 값으로 text 설정하기

--- react-base/src/App.js
+++ react-base/src/App.js
@@ -6,10 +6,12 @@ import ColorBar from './components/ColorBarComponent';
 
 function App() {
   const [inputColor, setInputColor] = useState('red');
+  const [text, setText] = useState('빠샤빠샤 화이팅');
+
 
   return (
     <div className="App" style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
-      <TodoInput inputColor={inputColor}/>
+      <TodoInput inputColor={inputColor} text={text} setText={setText}/>
       <ColorBar setInputColor={setInputColor}/>
     
   </div>
--- react-base/src/components/TodoInputComponent.js
+++ react-base/src/components/TodoInputComponent.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 
-export default function TodoInput({inputColor}) {
+export default function TodoInput({inputColor, text, setText}) {
 
-
+    const changeText = useCallback((e) => {
+        setText(e.target.value);
+    }, [text]);
 
     return (
         <div>
             <h1>Todo App</h1>
             <div>
-                <input style={{backgroundColor:inputColor}} type="text"/>
+                <input onChange={changeText} style={{backgroundColor:inputColor}} type="text"/>
                 <button>입력</button>
             </div>
         </div>
