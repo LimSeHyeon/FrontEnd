b89c54a1638be441ffccd77f5f3b04ad4d5a8443
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-12 17:30:46 +0900

    feat: 입력버튼 누르면 투두리스트 바꾸기

--- react-base/src/App.js
+++ react-base/src/App.js
@@ -7,11 +7,14 @@ import ColorBar from './components/ColorBarComponent';
 function App() {
   const [inputColor, setInputColor] = useState('red');
   const [text, setText] = useState('빠샤빠샤 화이팅');
-
+  const [todoList, setTodoList] = useState([{
+    'text': '세현아 잘하자',
+    'color': 'red'
+  }]);
 
   return (
     <div className="App" style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
-      <TodoInput inputColor={inputColor} text={text} setText={setText}/>
+      <TodoInput inputColor={inputColor} text={text} setText={setText} todoList={todoList} setTodoList={setTodoList}/>
       <ColorBar setInputColor={setInputColor}/>
     
   </div>
--- react-base/src/components/TodoInputComponent.js
+++ react-base/src/components/TodoInputComponent.js
@@ -1,17 +1,21 @@
 import React, {useCallback} from 'react';
 
-export default function TodoInput({inputColor, text, setText}) {
+export default function TodoInput({inputColor, text, setText, todoList, setTodoList}) {
 
     const changeText = useCallback((e) => {
         setText(e.target.value);
     }, [text]);
+
+    const addTodo = useCallback((e) => {
+        setTodoList([...todoList, {text: text, color: inputColor}]);
+    }, [text, inputColor, todoList]);
 
     return (
         <div>
             <h1>Todo App</h1>
             <div>
                 <input onChange={changeText} style={{backgroundColor:inputColor}} type="text"/>
-                <button>입력</button>
+                <button onClick={addTodo}>입력</button>
             </div>
         </div>
     )
