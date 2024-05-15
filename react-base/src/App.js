d54d2eac146c86c8b36ddc579ebf4cf98da416b8
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-16 08:36:50 +0900

    WIP: useContext로 훅 만들어 사용하기

--- react-base/src/App.js
+++ react-base/src/App.js
@@ -1,19 +1,22 @@
-import {useState, useEffect} from 'react';
-import TodoInput from './components/TodoInputComponent';
-import ColorBar from './components/ColorBarComponent';
-import TodoList from './components/TodolistComponent';
-import Search from './components/SearchComponent';
-import SearchResult from './components/SearchResultComponent';
-import {TodoStore} from './lib/utils';
+import { useState, useEffect } from "react";
+import TodoInput from "./components/TodoInputComponent";
+import ColorBar from "./components/ColorBarComponent";
+import TodoList from "./components/TodolistComponent";
+import Search from "./components/SearchComponent";
+import SearchResult from "./components/SearchResultComponent";
+import { TodoStore } from "./lib/utils";
+import TodoProvider from "./components/UseTodo";
 
 function App() {
-  const [inputColor, setInputColor] = useState('red');
-  const [text, setText] = useState('빠샤빠샤 화이팅');
-  const [todoList, setTodoList] = useState([{
-    'text': '세현아 잘하자',
-    'color': 'red'
-  }]);
-  const [searchText, setSearchText] = useState('');
+  const [inputColor, setInputColor] = useState("red");
+  const [text, setText] = useState("빠샤빠샤 화이팅");
+  const [todoList, setTodoList] = useState([
+    {
+      text: "세현아 잘하자",
+      color: "red",
+    },
+  ]);
+  const [searchText, setSearchText] = useState("");
   const [searchTodo, setSearchTodo] = useState([]);
 
   useEffect(() => {
@@ -21,15 +24,27 @@ function App() {
   }, []);
 
   return (
-    <div className="App" style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
-      <TodoInput inputColor={inputColor} text={text} setText={setText} todoList={todoList} setTodoList={setTodoList}/>
-      <ColorBar setInputColor={setInputColor}/>
-      <TodoList todoList={todoList}/>
-      <Search todoList={todoList} searchText={searchText} setSearchText={setSearchText} searchTodo={searchTodo} setSearchTodo={setSearchTodo} />
-      <SearchResult searchTodo={searchTodo}/>
-      
-    
-  </div>
+    <TodoProvider>
+      <div className="App" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <TodoInput
+          inputColor={inputColor}
+          text={text}
+          setText={setText}
+          todoList={todoList}
+          setTodoList={setTodoList}
+        />
+        <ColorBar setInputColor={setInputColor} />
+        <TodoList todoList={todoList} />
+        <Search
+          todoList={todoList}
+          searchText={searchText}
+          setSearchText={setSearchText}
+          searchTodo={searchTodo}
+          setSearchTodo={setSearchTodo}
+        />
+        <SearchResult searchTodo={searchTodo} />
+      </div>
+    </TodoProvider>
   );
 }
 
--- react-base/src/components/UseTodo.js
+++ react-base/src/components/UseTodo.js
@@ -1,18 +1,49 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { createContext, useState } from "react";
+import {v4 as uuidv4 } from "v4";
+import { TodoStore } from "../../lib/utils";
 
-export default useTodo = () => {
-  const [inputColor, setInputColor] = useState("red");
-  const [text, setText] = useState("빠샤빠샤 화이팅");
-  const [todoList, setTodoList] = useState([
-    {
-      text: "세현아 잘하자",
-      color: "red",
+export const todoContext = createContext();
+
+export default function TodoProvider({children}) {
+
+  //TodoList 기능 정의
+  const [todoList, setTodoList] = useState([]);
+
+  // addTodo
+  const addTodo = useCallback(
+    (text, color) => {
+      const todos = [...todoList, { id: uuidv4(), text, color }];
+      setTodoList(todos);
+      TodoStore.setTodo(todos);
     },
-  ]);
+    [todoList],
+  );
+  const removeTodo = useCallback(
+    (todoId) => {
+      const newTodoList = todoList.filter((todo, idx) => todoId !== todo.id);
+      setTodoList(newTodoList);
+      TodoStore.setTodo(newTodoList);
+    },
+    [todoList],
+  );
+  const editTodo = useCallback(
+    (todoId, text) => {
+      const newTodoList = todoList.map((todo) => {
+        if (todo.id === todoId) {
+          return {
+            ...todo,
+            text,
+          };
+        }
+        return todo;
+      });
+      setTodoList(newTodoList);
+      TodoStore.setTodo(newTodoList);
+    },
+    [todoList],
+  );
 
 
-  const addTodo = (todo) => {
-    setTodoList([...todoList, todo])
-  };
+  return <todoContext.Provider value={todo}>{children}</todoContext.Provider>;
+}
 
-};
